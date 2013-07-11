5c54efd4bf36191c16d31a394675073e942a5ccb
cheongchan <cheong.chan@yale.edu>
2013-07-11 10:54:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drawing::Application.config.secret_key_base = 'f128480508b09ad3e1784df796a66479192dc375fcedc0a2fc7fb12a112973e497f55ab81e4d5ff2535d4a65c071e25bf83021fd6c94348cb4621bd98c427443'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Drawing::Application.config.session_store :cookie_store, key: '_Drawing_session'
