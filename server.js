76670def911bb293e2acae2fc2d05bda0c11aa9a
enylund <eric.nylund@yale.edu>
2013-07-16 13:10:56 -0400

    Add basic collaborative drawing capabilities.

--- server.js
+++ server.js
@@ -1,58 +1,20 @@
 var io = require('socket.io'),
   connect = require('connect');
-
-var messagelog = [];
-
-var storeMessages = function (name, data) {
-  messagelog.push({name: name, data: data});
-  if (messagelog.length > 20) messagelog.shift();
-}
+var paper = require('paper');
 
 var app = connect().use(connect.static('public')).listen(8888);
 
-var chat_room = io.listen(app);
-var connectCounter = 0;
+var drawing_room = io.listen(app);
 
-chat_room.sockets.on('connection', function(socket) {
+drawing_room.sockets.on('connection', function(socket) {
 
-  connectCounter++;
-  console.log(connectCounter);
+  console.log(socket.id);
 
-  chat_room.sockets.emit('changeCount', {message: connectCounter});
-
-  messagelog.forEach(function(message) { 
-    socket.emit('chat', {message: message.name + ": " + message.data});
-  });
-
-  messagelog.forEach(function(message) {
-    socket.emit('names', {message: message.name});
-  });
-
-  socket.emit('entrance', {message: "welcome to the chat"});
-  socket.broadcast.emit('entrance', {message: "A new chatter is online"});
-
-  socket.on('addname', function(nickname) {
-
-    socket.set('nickname', nickname);
-
+  socket.on('newpoint', function(data) {
+    socket.broadcast.emit('broadcastnewpoint', {new: data});
   });
 
   socket.on('disconnect', function() {
-    chat_room.sockets.emit('exit', {message: "Someone left"});
-    connectCounter--;
-    console.log(connectCounter);
-    chat_room.sockets.emit('changeCount', {message: connectCounter});
   });
 
-  socket.on('chat', function(data) {
-    socket.get('nickname', function(err, name) {
-      if(name == null) name = "anonPnda";
-        chat_room.sockets.emit('chat', {message: name + ": " + data.message});
-        storeMessages(name, data.message);
-        console.log(messagelog);
-    });
-  });
 });
-
-
-
