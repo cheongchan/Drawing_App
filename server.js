bed7155ef0112505ab51dc932970b7a7194305ca
enylund <eric.nylund@yale.edu>
2013-07-19 14:05:49 -0400

    add collaborative drawing functionality.

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ var drawing_room = io.listen(app);
 
 drawing_room.sockets.on('connection', function(socket) {
 
-  console.log(socket.id);
-
-  socket.on('newpoint', function(data) {
-    socket.broadcast.emit('broadcastnewpoint', {new: data});
+  socket.on('onMouseDown', function(data) {
+    socket.broadcast.emit('onMouseDown', data);
   });
-
-  socket.on('disconnect', function() {
+  socket.on('onMouseDrag', function(data) {
+    socket.broadcast.emit('onMouseDrag', data);
+  });
+  socket.on('onMouseUp', function(data) {
+    socket.broadcast.emit('onMouseUp', data);
   });
 
 });
