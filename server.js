95cd0a0af170593219fd6aee50d51a30c210acbb
cheongchan <cheong.chan@yale.edu>
2013-07-22 15:42:56 -0400

    Added basic importing functionality.

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ var app = connect().use(connect.static('public')).listen(8888);
 
 var drawing_room = io.listen(app);
 
-drawing_room.sockets.on('connection', function(socket) {
+//Testing Persistance
+var pictures1;
+var pictures2;
 
+drawing_room
+  .of('/drawing1')
+  .on('connection', function(socket) {
+    socket.emit('import', pictures1);
   socket.on('onMouseDown', function(data) {
     socket.broadcast.emit('onMouseDown', data);
   });
@@ -17,5 +23,32 @@ drawing_room.sockets.on('connection', function(socket) {
   socket.on('onMouseUp', function(data) {
     socket.broadcast.emit('onMouseUp', data);
   });
+  socket.on('export', function(data) {
+    socket.broadcast.emit('export', data);
+    pictures1 = data;
+    console.log("EXPORT________");
+    console.log(data);
+  });
 
+});
+
+drawing_room
+  .of('/drawing2')
+  .on('connection', function(socket) {
+    socket.emit('import', pictures2);
+  socket.on('onMouseDown', function(data) {
+    socket.broadcast.emit('onMouseDown', data);
+  });
+  socket.on('onMouseDrag', function(data) {
+    socket.broadcast.emit('onMouseDrag', data);
+  });
+  socket.on('onMouseUp', function(data) {
+    socket.broadcast.emit('onMouseUp', data);
+  });
+  socket.on('export', function(data) {
+    socket.broadcast.emit('export', data);
+    pictures2 = data;
+    console.log("EXPORT________");
+    console.log(data);
+  });
 });
