bed8eb463bbb48e38cbbf98aa124864a7b8e5087
cheongchan <cheong.chan@yale.edu>
2013-07-23 16:01:52 -0400

    Implemented a simple persistence which works only if node server is up

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var io = require('socket.io'),
 var app = connect().use(connect.static('public')).listen(8888);
 
 var drawing_room = io.listen(app);
+var picture = new Object();
 
 drawing_room
   .on('connection', function(socket) {
+  socket.on('channel', function(data) {
+    //imports
+    channel_id = data;
+    socket.emit('import', picture[channel_id]); 
+  });  
   socket.on('onMouseDown', function(data) {
     socket.broadcast.emit("onMouseDown"+data.channel, data);
   });
@@ -16,5 +22,11 @@ drawing_room
   });
   socket.on('onMouseUp', function(data) {
     socket.broadcast.emit("onMouseUp"+data.channel, data);
+  });
+  socket.on('update',function(data){
+    console.log(data);
+    picture[data.channel] = data.JSON;
+    console.log('le')
+    console.log(picture);
   });
 });
