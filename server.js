7980af6d8a8f6502b26b27eb29a241d176a0ec88
cheongchan <cheong.chan@yale.edu>
2013-07-24 13:27:58 -0400

    Added nonpersistent SVG save to pages

--- server.js
+++ server.js
@@ -9,24 +9,31 @@ var picture = new Object();
 
 drawing_room
   .on('connection', function(socket) {
+
+  //imports info for each show page
   socket.on('channel', function(data) {
-    //imports
     channel_id = data;
     socket.emit('import', picture[channel_id]); 
   });  
+
   socket.on('onMouseDown', function(data) {
     socket.broadcast.emit("onMouseDown"+data.channel, data);
   });
+
   socket.on('onMouseDrag', function(data) {
     socket.broadcast.emit("onMouseDrag"+data.channel, data);
   });
+
   socket.on('onMouseUp', function(data) {
     socket.broadcast.emit("onMouseUp"+data.channel, data);
   });
+
+  socket.on('SVGadd',function(data){
+    socket.broadcast.emit("SVGadd"+data.channel, 'hello');
+  });
+
+  //gets JSON data and puts it into variable
   socket.on('update',function(data){
-    console.log(data);
     picture[data.channel] = data.JSON;
-    console.log('le')
-    console.log(picture);
   });
 });
